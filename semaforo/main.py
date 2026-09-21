bb619cde0f2688431d5d45755f9ed7392fec2f66
Rcandido42 <deepcandido@gmail.com>
2026-09-21 19:09:54 +0100

    Comentarios

--- semaforo/main.py
+++ semaforo/main.py
@@ -1,9 +1,15 @@
 import tkinter
 
+#Definindo as variaveis da janela
 janela = tkinter.Tk()
-
 janela.title("Simulador de semáforo")
-
 janela.geometry("400x600")
+
+#tkinter.Canvas(...) cria a área de desenho
+#janela diz onde o canvas vai ficar
+#width=300 define a largura
+#height=500 define a altura
+canvas = tkinter.Canvas(janela, width=300, height=500)
+
 
 janela.mainloop()
