c625cbaf58dc6d0d571fce9ca4524aa31aa4e206
Rcandido42 <deepcandido@gmail.com>
2026-09-21 19:40:26 +0100

    Implementar a lógica de mudança de cores do semáforo

--- semaforo/main.py
+++ semaforo/main.py
@@ -19,5 +19,34 @@ luz_amarela = canvas.create_oval(110, 170, 190, 250, fill="yellow")
 #Criando a luz verde
 luz_verde = canvas.create_oval(110, 270, 190, 350, fill="green")
 
+#Logica de mudar as cores
+
+#vermelho
+def mostrar_vermelho ():
+    canvas.itemconfig(luz_vermelha, fill="red")
+    canvas.itemconfig(luz_amarela, fill="gray")
+    canvas.itemconfig(luz_verde, fill="gray")
+    janela.after(5000, mostrar_verde())
+
+#amarelo
+def mostrar_amarelo ():
+    canvas.itemconfig(luz_vermelha, fill="gray")
+    canvas.itemconfig(luz_amarela, fill="yellow")
+    canvas.itemconfig(luz_verde, fill="gray")
+    janela.after(5000, mostrar_amarelo())
+
+#verde
+def mostrar_verde ():
+    canvas.itemconfig(luz_vermelha, fill="gray")
+    canvas.itemconfig(luz_amarela, fill="gray")
+    canvas.itemconfig(luz_verde, fill="green")
+    janela.after(5000, mostrar_vermelho())
+    
+
+
+
+
+
+
 
 janela.mainloop()
